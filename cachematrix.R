6146f173e9a3a11887d76ecc75e13e0ead70d946
Michael Lee <michael.h.lee.civ@mail.mil>
2016-08-24 11:29:39 -0400

    Commit completed cachematrix.R assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,72 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 8/23/2016
+## R-Programming - Programming assignment 2: Caching the Inverse of a Matrix.
+## Write a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
 
+## The 'makeCacheMatrix' function creates a special "matrix" object that can cache its inverse.
+## Return a list containing functions set, get, setInverse, getInverse.
 makeCacheMatrix <- function(x = matrix()) {
+  mInverse <- NULL
 
+  # set the value of the new matrix
+  set <- function(matrix) {
+    x <<- matrix
+    mInverse <<- NULL
+  }
+  
+  # get the value of the matrix
+  get <- function(){
+    x
+  } 
+  
+  # set the value of the inverse
+  setInverse <- function(inverse){
+    mInverse <<- inverse
+  } 
+  
+  # get the value of the inverse
+  getInverse <- function(){
+    mInverse
+  } 
+  
+  
+  # Prepare list containing a functions set, get, setInverse, getInverse
+  list(set = set, 
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
+## The 'cacheSolve' function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
+##
+## Computing the inverse of a square matrix can be done with the solve function in R. 
+## For example, if X is a square invertible matrix, then solve(X) returns its inverse.
+## Assume that the matrix supplied is always invertible.
+##
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  # Attempt to get the inverse value stored in x.
+  inverse <- x$getInverse()
+  
+  if(!is.null(inverse)) {
+    # Cached data was found. We don't have to calculate the inverse here.
+    message("getting cached data")
+    return(inverse)
+  }
+  
+  # There was no cached data. We have to calculate the matrix inverse.
+  myMatrix <- x$get()
+  inverse <- solve(myMatrix, ...)
+  x$setInverse(inverse)
+  
+  # Test
+  #testAnswer <- myMatrix %*% inverse
+  #print(testAnswer)   # Test to see if inverse is correct. We should get 1.
+  
+  inverse
 }
